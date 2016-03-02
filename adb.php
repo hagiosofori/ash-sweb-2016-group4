075158cefe581b6c1fb746ea8745368c5152017f
Alvin Ofori <Alvin Ofori>
2016-03-02 23:48:13 +0000

    initial skeleton program files

---
+++ adb.php
@@ -0,0 +1,34 @@
+<?php
+	include_once("settings.php");
+	
+	$conn= null;//the connection to the database
+	$data= null;
+	/*
+	* connects to database
+	*/
+	function connect()
+	{
+		
+	}
+	
+	/*
+	* queries the database
+	* checks if the connection variable is not null first...
+	* results of the query are stored in the data variable
+	* @param string query
+	* @return boolean indicating whether the query was successful or not
+	*/
+	function query($strQuery)
+	{
+		
+	}
+	
+	/*
+	* @return one row of the results of the query
+	*/
+	function fetch()
+	{
+		
+	}
+	
+?>
---
+++ users.php
@@ -0,0 +1,66 @@
+<?php
+include_once("adb.php");
+
+	class users extends adb
+	{
+		/*
+		* adds new user to the database
+		* @param [all attributes needed to create a user]
+		* @returns boolean showing success or failure
+		*/
+		function addNewUser(/* parameters needed to create new user*/)
+		{
+			
+		}
+		
+		/*
+		* edits existing user in the database
+		* @param [all attributes of a user]
+		* @return boolean repersenting success or failure
+		*/
+		function editUser(/*same parameters as above */)
+		{
+			
+		}
+		
+		/*
+		* delete user from database
+		* @param primary key of user's table
+		* @return boolean representing success or failure
+		*/
+		function deleteUser(/* primary key of user table*/)
+		{
+			
+		}
+		
+		/*
+		* get user, with primary key 
+		* @param primary key of user's table
+		* @return row of user's attributes
+		*/
+		function getUser(/*primary key of users table*/)
+		{
+			
+		}
+		
+		/*
+		* logs the user in, given accurate credentials
+		* @param: user's login credentials
+		* @return: boolean based on success or failure
+		*/
+		function login(/*user's login credentials */)
+		{
+			
+		}
+		
+		/*
+		* toggles the availability of the user whose id is passed as parameter
+		* @param: user's id, or other unique identifier
+		* @return: the new availability of the user
+		*/
+		function toggleAvailability(/*primary identifier of user */){
+			
+		}
+	}
+
+?>
