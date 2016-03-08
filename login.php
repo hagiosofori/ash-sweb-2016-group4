45efa5846626533ac165e8cc91c024162ed2e834
[KBoohene] <[kwabena.boohene@ashesi.edu.gh]>
2016-03-08 08:19:23 +0000

    Created basic login interface

---
+++ login.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+  
+<?php
+include_once("users.php");
+
+?>
+
+<form action="" method="GET" onsubmit='validate()'>
+  <div>Username: <input type="text" name="username" value=""/></div>
+  <div>Password: <input type="password" name="password" value=""/></div>
+<input type="submit" name="Login" value="Login">
+
+</form>
+</body>
+</html>
