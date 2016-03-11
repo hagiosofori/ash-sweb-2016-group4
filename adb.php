7b44831145937ed50183e5677b61293d772da2c3
[KBoohene] <[kwabena.boohene@ashesi.edu.gh]>
2016-03-11 19:41:37 +0000

    added fetch method

--- adb.php
+++ adb.php
@@ -1,34 +1,76 @@
 <?php
-	include_once("settings.php");
-	
-	$conn= null;//the connection to the database
-	$data= null;
-	/*
-	* connects to database
-	*/
-	function connect()
-	{
-		
+/**
+*Database connection helper
+*/
+include_once("settings.php");
+/**
+* Database connection helper class
+*/
+class adb{
+	var $db=null;
+	var $result=null;
+	function adb(){
 	}
-	
-	/*
-	* queries the database
-	* checks if the connection variable is not null first...
-	* results of the query are stored in the data variable
-	* @param string query
-	* @return boolean indicating whether the query was successful or not
+	/**
+	*Connect to database
+	*@return boolean ture if connected else false
 	*/
-	function query($strQuery)
-	{
-		
+	function connect(){
+
+		//connect
+		$this->db=new mysqli(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
+		if($this->db->connect_errno){
+			//no connection, exit
+			return false;
+		}
+		return true;
 	}
-	
-	/*
-	* @return one row of the results of the query
+
+	/**
+	*Query the database
+	*@param string $strQuery sql string to execute
 	*/
-	function fetch()
-	{
-		
+	function query($strQuery){
+		if(!$this->connect()){
+			return false;
+		}
+		if($this->db==null){
+			return false;
+		}
+		$this->result=$this->db->query($strQuery);
+		if($this->result==false){
+			return false;
+		}
+		return true;
 	}
-	
+
+
+	/*
+	* Fetch from the current data set and return
+	*@return array one record
+	*/
+	function fetch(){
+		//Complete this funtion to fetch from the $this->result
+		if($this->result==null){
+			return false;
+		}
+
+		if($this->result==false){
+			return false;
+		}
+
+		return $this->result->fetch_assoc();
+	}
+
+}
+/*
+This is a test code
+$obj=new adb();
+if(!$obj->query("select * from users"))
+{
+	echo "error";
+	exit();
+}
+print_r($obj->fetch());
+*/
 ?>
