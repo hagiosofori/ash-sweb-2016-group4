c511292432f11540c24f67cd219a398f64b3b995
[KBoohene] <[kwabena.boohene@ashesi.edu.gh]>
2016-03-16 23:48:11 +0000

    added admin and user options to page

--- signup.php
+++ signup.php
@@ -8,6 +8,9 @@
   <div>Firstname: <input type="text" name="firstname" value=""/></div>
   <div>Lastname: <input type="text" name="lastname" value=""/></div>
   <div>Password: <input type="password" name="password" value=""/></div>
+  <div>Status <input type="radio" name="status" value="1"> Admin
+  <input type="radio" name="status" value="0"> User
+  </div>
 <input type="submit" name="signUp" value="Sign Up">
 
 <?php
@@ -23,11 +26,12 @@ $user = new users();
 	$lastname=$_REQUEST['lastname'];
 	$password=$_REQUEST['password'];
 
+//Stores the boolean result in a variable
   $verify = $user->addNewUser($username,$firstname,$lastname,$password);
 
   if($verify==true){
     echo "user added";
-  
+
   }
   else{
     echo "User was not created";
