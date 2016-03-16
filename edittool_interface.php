c554614422e74e286f51cd494fc6f04581dc6e5d
ephraimayite <ephraimayite@gmail.com>
2016-03-16 23:40:38 +0000

    added interfaces for editing

--- edittool_interface.php
+++ edittool_interface.php
@@ -1,23 +1,51 @@
 
-<?php
+	<?php
 
-echo"<form action=\"editdrug_interface.php\" method=\"GET\">";
-echo"<input type=\"hidden\" name=\"toolId\Id\" value=  >";
-echo"<input type=\"hidden\" name=\"toolName\" value=  >";
-echo"<input type=\"text\" name=\"quantity\" value=  >";
-echo"<input type=\"text\" name=\"supplierId\" value=  >";
-echo"<input type=\"text\" name=\"toolType\" value=  >";
-echo"<input type=\"submit\" name=\"submit\" value=\"ClicktoEdit\">";
+	include ("tools.php");
 
-if(isset($_REQUEST['submit'])){
-$toolId = $_REQUEST['toolId'];
-$toolname = $_REQUEST['toolName'];	
-$quantity = $_REQUEST['quantity'];
-$suppliers = $_REQUEST['suppliersId'];
-$toolType = $_REQUEST['toolType'];
+	$toolId = '';
+	$toolname = '';	
+	$quantity ='' ;
+	$suppliers ='' ;
+	$toolType ='';
 
-$tool  = new tools();
-$tool->editTool($toolId,$toolName,$quantity,$suppliers,$toolType,"toolType");	
-}
+	if(isset($_REQUEST['submit'])){
+	$toolId = $_REQUEST['toolId'];
+	$toolname = $_REQUEST['toolName'];	
+	$quantity = $_REQUEST['quantity'];
+	$suppliers = $_REQUEST['suppliersId'];
+	$toolType = $_REQUEST['toolType'];
 
-?>
+
+	$tool  = new tools();
+	$tool->editTool($toolId,$toolName,$quantity,$suppliers,$toolType,"Tools");
+
+	echo"<div><form action=\"editdrug_interface.php\" method=\"GET\"></div>";
+	echo"<div><input type=\"hidden\" name=\"toolId\Id\" value=  ></div>";
+	echo"<div><input type=\"hidden\" name=\"toolName\" value=  ></div>";
+	echo"<div><input type=\"text\" name=\"quantity\" value=  ></div>";
+	echo"<div><input type=\"text\" name=\"supplierId\" value=  ></div>";
+	echo"<div><input type=\"text\" name=\"toolType\" value=  ></div>";
+	echo"<div><input type=\"submit\" name=\"submit\" value=\"ClicktoEdit\">";	
+	}
+
+	if(isset($_REQUEST['id'])){
+		$tool = new Tools();
+		$id = $_REQUEST['id'];
+		$tool->searchTools($id);
+		$row =$tool->fetch();
+		$toolName =$row['toolName'];
+	$Quantity=$row['quantity'];
+	$supplierId=$row['supplierId'];
+	$toolType=$row['toolType'];
+
+	echo"<div><form action=\"editdrug_interface.php\" method=\"GET\"></div>";
+	echo"<div><input type=\"hidden\" name=\"toolId\Id\" value=  ></div>";
+	echo"<div><input type=\"hidden\" name=\"toolName\" value=  ></div>";
+	echo"<div><input type=\"text\" name=\"quantity\" value=  ></div>";
+	echo"<div><input type=\"text\" name=\"supplierId\" value=  ></div>";
+	echo"<div><input type=\"text\" name=\"toolType\" value=  ></div>";
+	echo"<div><input type=\"submit\" name=\"submit\" value=\"ClicktoEdit\">";
+		
+	}
+	?>
