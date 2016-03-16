f6ffcdced938b7e7f844b28466a71141ecf4cf1a
ryjaled <ryjaled@gmail.com>
2016-03-16 11:03:25 +0000

    input function to get tools to populate report

--- report.php
+++ report.php
@@ -35,6 +35,16 @@ function runMyFunction(){
 	}
 
 
+  //He has this as a capital leter TOOLS which is different form drugs verify this.
+  function getTools($filter=false){
+    $strQuery="select toolID, toolName, quantity, supplierID, toolType from Tools ";
+      // $strQuery="select * from users";
+    if($filter!=false){
+      $strQuery=$strQuery . " where $filter";
+    }
+    return $this->query($strQuery);
+  }
+
 
  <a href ='report.php?hello=true'>Generate</a>
 </html>
