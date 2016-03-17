b9ff97a772f2d58e4b9787d5ff4f600fe8e64434
ryjaled <ryjaled@gmail.com>
2016-03-17 00:17:24 +0000

    made report generate in formatted tables for drugs and tools

--- generatereport.php
+++ generatereport.php
@@ -1,57 +1,64 @@
-<!--
-<?php
-if(!isset($_GET['st'])){
-$result=$mysqli->query("select * from userinfo");
-
-}
-
-//searching for a whole or part of a username within the database to select a record
-if($id=$_GET['st']){
-  $result=$mysqli->query("select drugID, drugName, quantity, supplierID, drugType from drugs");
-}
-//sorting the usernames in the database by alphabetical order. 1 for Ascending and 2 for Descending.
-  if($id=$_GET['sort']){
-
-   if($id==1){
-     $result=$mysqli->query("select toolID, toolName, quantity, supplierID, toolType from Tools ");
-   }
-   if($id==2){
-     echo "some stuff to add";
-    //  $result=$mysqli->query("select * from USERS ORDER by USERNAME DESC");
-   }
-}
-
-if ($result->num_rows > 0) {
-
-echo "<table border = '1'>";
-
-  while($row = $result->fetch_assoc()) {
-
-    echo "<tr>
-              <td style='background-color:#6699ee'>{$row['USERNAME']}</td>
-              <td style='background-color:#6699ee'>{$row['FIRSTNAME']}</td>
-              <td style='background-color:#6699ee'>{$row['LASTNAME']}</td>
-              <td style='background-color:#6699ee'>{$row['USERGROUP']}</td>
-              <td style='background-color:#6699ee'>{$row['STATUS']}</td>
-          </tr>";
-
-        }
-
-echo "</table>";
-
-  }
-
-
-?> -->
-
+<html>
+<head>
+</head>
+<body>
 <?php
 
 include_once("report.php");
-$obj = new users();
-$r = $obj->getDrugs();
-if(!$r){
-  echo "error getting users";
+$obj = new report();
+
+if($id = $_GET['preference']){
+
+  if($id==1){
+    $r = $obj->getDrugs();
+
+
+    echo "<table width = 30% border='1'>";
+    while($row=$obj->fetch()){
+    // $row=$obj->fetch();
+
+
+    echo"<tr>
+        <td>{$row['drugID']}</td>
+        <td>{$row['drugName']}</td>
+        <td>{$row['quantity']}</td>
+        <td>{$row['supplierID']}</td>
+        <td>{$row['drugType']}</td>
+        </tr>";
+
+    }
+
+    echo "</table>";
+
+  }
+
+  if($id==2){
+    $r = $obj->getTools();
+
+
+    echo "<table width = 30% border='1'>";
+    while($row=$obj->fetch()){
+    // $row=$obj->fetch();
+
+
+    echo"<tr>
+        <td>{$row['toolId']}</td>
+        <td>{$row['toolName']}</td>
+        <td>{$row['quantity']}</td>
+        <td>{$row['supplierId']}</td>
+        <td>{$row['toolType']}</td>
+        </tr>";
+
+    }
+
+    echo "</table>";
+  }
+
+}
+else if(!$r){
+  echo "error getting data";
 }
 
-
 ?>
+</body>
+</html>
--- report.php
+++ report.php
@@ -1,57 +1,3 @@
-
-<!-- <html> -->
-//php tag shuold open here
-// $myfile = fopen("report.txt", "w") or die("Unable to open file!");
-
-// if(isset($_GET['hello'])){
-//   nurseInfoFunction();
-// }
-// else if(isset($_GET['drug'])){
-//   drugReport();
-// }
-// else if (isset($_GET['tool'])){
-//   toolReport();
-// }
-//
-//
-// function drugReport(){
-// $r = $obj->getDrugs();
-// if(!$r){
-//   echo "error getting drugs";
-// }
-//
-// $myfile = fopen("report.txt", "w") or die("Unable to open file!");
-//
-// while($row=$obj->fetch()){
-//   $drug = "{$row['drugID']}"+"{$row['drugName']}"+"{$row['quantity']}"+"{$row['supplierID']}"+"{$row['drugType']}";
-//   fwrite($myfile, $drug);
-//   }
-//   fclose($myfile);
-// }
-//
-// function toolReport(){
-// $r = $obj->getTools();
-// if(!$r){
-//   echo "error getting tools";
-// }
-//
-// $myfile = fopen("report.txt", "w") or die("Unable to open file!");
-//
-// while($row=$obj->fetch()){
-//   $tool = "{$row['toolID']}"+"{$row['toolName']}"+"{$row['quantity']}"+"{$row['supplierID']}"+"{$row['toolType']}";
-//   fwrite($myfile, $tool);
-//   }
-//   fclose($myfile);
-}
-
-
-//might be redundant
-// if(isset($_GET['hello'])){
-//   runMyFunction();
-// }
-
-//php tag should close here
-
 <?php
 include_once("adb.php");
 
@@ -68,7 +14,7 @@ function nurseInfoFunction(){
 
 
   function getDrugs($filter=false){
-		$strQuery="select drugID, drugName, quantity, supplierID, drugType from drugs ";
+		$strQuery='select drugID, drugName, quantity, supplierID, drugType from drugs';
 			// $strQuery="select * from users";
 		if($filter!=false){
 			$strQuery=$strQuery . " where $filter";
@@ -79,7 +25,7 @@ function nurseInfoFunction(){
 
   //He has this as a capital leter TOOLS which is different form drugs verify this.
   function getTools($filter=false){
-    $strQuery="select toolID, toolName, quantity, supplierID, toolType from Tools ";
+    $strQuery='select toolId, toolName, quantity, supplierId, toolType from Tools';
       // $strQuery="select * from users";
     if($filter!=false){
       $strQuery=$strQuery . " where $filter";
