012bea57d12434444aa8ca9eb094d2f6db2356da
[KBoohene] <[kwabena.boohene@ashesi.edu.gh]>
2016-03-17 21:07:16 +0000

    commented login class

--- login.php
+++ login.php
@@ -1,35 +1,44 @@
 <html>
-<body>
+  <head>
+    <title>login</title>
+  </head>
 
+  <body>
+    <!--Form to collect information to login-->
+    <form action="" method="GET" onsubmit='validate()'>
+      <div>Username: <input type="text" name="username" value=""/></div>
+      <div>Password: <input type="password" name="password" value=""/></div>
+      <input type="submit" name="Login">
 
+      <?php
+      //Included the user class
+        include_once("users.php");
 
-<form action="" method="GET" onsubmit='validate()'>
-  <div>Username: <input type="text" name="username" value=""/></div>
-  <div>Password: <input type="password" name="password" value=""/></div>
-<input type="submit" name="Login">
+      //Created a user object
+        $user = new users();
 
-<?php
-include_once("users.php");
-$user = new users();
+        if(!isset($_REQUEST['username'])){
+          exit();		//if no data, exit
+        }
 
-if(!isset($_REQUEST['username'])){
-  exit();		//if no data, exit
-}
+        //Stores the users information
+        $username=$_REQUEST['username'];
+        $password=$_REQUEST['password'];
 
-$username=$_REQUEST['username'];
-$password=$_REQUEST['password'];
+        //Calls the login methods
+        $verify=$user->login($username,$password);
 
-$verify=$user->login($username,$password);
+        //Displays the whether user exists or not
+        if($verify==false){
+          echo"user does not exist";
+        }
+        else{
+          echo "verified";
+        /* header("Location:check.php?userType={$verify["userType"]}");
+        exit();*/
+        }
+      ?>
+    </form>
 
-if($verify==false){
-  echo"user does not exist";
-}
-else{
-  echo "verified";
-/*  header("Location:check.php?userType={$verify["userType"]}");
-  exit();*/
-}
-?>
-</form>
-</body>
+  </body>
 </html>
