8db52e5f4e7088b4958f67e67131b7f984a467dd
[KBoohene] <[kwabena.boohene@ashesi.edu.gh]>
2016-03-17 20:54:15 +0000

    commented signup class

--- signup.php
+++ signup.php
@@ -1,44 +1,53 @@
 <html>
-<body>
+  <head>
+    <title>Sign Up</title>
+  </head>
+  <body>
 
+    <!-- Form used to collect information about the user-->
+    <form action="" method="GET" onsubmit='validate()'>
+      <div>Username: <input type="text" name="username" value=""/></div>
+      <div>Firstname: <input type="text" name="firstname" value=""/></div>
+      <div>Lastname: <input type="text" name="lastname" value=""/></div>
+      <div>Password: <input type="password" name="password" value=""/></div>
+      <div>
+        User Type: <input type="radio" name="userType" value="1"> Admin
+        <input type="radio" name="userType" value="0"> User
+      </div>
+      <input type="submit" name="signUp" value="Sign Up">
 
+      <?php
+       //Included users class
+        include_once("users.php");
 
-<form action="" method="GET" onsubmit='validate()'>
-  <div>Username: <input type="text" name="username" value=""/></div>
-  <div>Firstname: <input type="text" name="firstname" value=""/></div>
-  <div>Lastname: <input type="text" name="lastname" value=""/></div>
-  <div>Password: <input type="password" name="password" value=""/></div>
-  <div> User Type: <input type="radio" name="userType" value="1"> Admin
-  <input type="radio" name="userType" value="0"> User
-  </div>
-<input type="submit" name="signUp" value="Sign Up">
+        //Created instance of user object
+        $user = new users();
 
-<?php
-include_once("users.php");
-$user = new users();
+        //Checks if username variable is available
+	      if(!isset($_REQUEST['username'])){
+	         exit();
+	      }
 
-	if(!isset($_REQUEST['username'])){
-		exit();
-	}
+        //Stores the basic user information
+        $username=$_REQUEST['username'];
+	      $firstname=$_REQUEST['firstname'];
+	      $lastname=$_REQUEST['lastname'];
+	      $password=$_REQUEST['password'];
+        $type=$_REQUEST['userType'];
 
-	$username=$_REQUEST['username'];
-	$firstname=$_REQUEST['firstname'];
-	$lastname=$_REQUEST['lastname'];
-	$password=$_REQUEST['password'];
-  $type=$_REQUEST['userType'];
+        //Calls the addNewUser method
+        $verify = $user->addNewUser($username,$firstname,$lastname,$password,$type);
 
-//Stores the boolean result in a variable
-  $verify = $user->addNewUser($username,$firstname,$lastname,$password,$type);
-
-  if($verify==true){
-    echo "user added";
-    /*header("Location:index.php?userType=$type");
-    exit();*/
-  }
-  else{
-    echo "User was not created";
-  }
-?>
-</form>
-</body>
+        //Displays verfication message based on result
+        if($verify==true){
+          echo "user added";
+          /*header("Location:index.php?userType=$type");
+          exit();*/
+        }
+        else{
+          echo "User was not created";
+        }
+      ?>
+    </form>
+  </body>
 </html>
