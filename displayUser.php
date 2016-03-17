e3cad3331d33a9c853d4ff9ae173e961d4940e4e
[KBoohene] <[kwabena.boohene@ashesi.edu.gh]>
2016-03-17 00:48:30 +0000

    got the page to display user information

--- displayUser.php
+++ displayUser.php
@@ -3,6 +3,33 @@
 <body>
   <?php
   include_once("users.php");
+  $user = new users();
+  $row = $user->getUser();
+
+  	echo"<table border=1>
+  				<tr>
+  					<td>USERNAME</td>
+  					<td>FIRSTNAME</td>
+  					<td>LASTNAME</td>
+  					<td>USER TYPE</td>
+            <td>OPTIONS</td>
+  				</tr>";
+	while($row=$user->fetch()){
+        echo"  <tr>
+  					<td>{$row['username']}</td>
+  					<td>{$row['firstname']}</td>
+  					<td>{$row['lastname']}</td>
+  					<td>{$row['userType']}</td>
+  					<td><a href='userDel.php?usercode={$row["userID"]}'>
+  					Delete</a>
+            </td>
+  					<td><a href='usersedit.php?usercode={$row["userID"]}'>
+  					Edit</a>
+  					</td>
+
+  			</tr>";
+  }
+  echo"</table>";
   ?>
 </body>
 </html>
