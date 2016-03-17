3f65401351c49dc5cfbca78c1d38a6b157858ec9
[KBoohene] <[kwabena.boohene@ashesi.edu.gh]>
2016-03-17 01:39:48 +0000

    displayed info stored in the database

--- editUser.php
+++ editUser.php
@@ -4,24 +4,29 @@
 
 </head>
 <body>
+	<?php
+	include_once("users.php");
+	if(isset($_REQUEST['userID']))
+	{
+		$user = new users();
+		$code=$_REQUEST['userID'];
+		$info = $user->getUser($code);
+		$info = $user->fetch();
+	}
+	?>
 
 	<form action="" method="GET" onsubmit='validate()'>
-  <div>Username: <input type="text" name="username" value=""/></div>
-  <div>Firstname: <input type="text" name="firstname" value=""/></div>
-  <div>Lastname: <input type="text" name="lastname" value=""/></div>
-  <div>Password: <input type="password" name="password" value=""/></div>
-  <div> User Type: <input type="radio" name="userType" value="1"> Admin
-  <input type="radio" name="userType" value="0"> User
+	<input type="hidden" name="usercode" value="<?php echo $info['userID']?>">
+  <div>Username: <input type="text" name="username" value="<?php echo $info['username']?>"/></div>
+  <div>Firstname: <input type="text" name="firstname" value="<?php echo $info['firstname']?>"/></div>
+  <div>Lastname: <input type="text" name="lastname" value="<?php echo $info['lastname']?>"/></div>
+  <div>Password: <input type="password" name="password" value="<?php echo $info['password']?>"/></div>
 	</div>
-  <input type="submit" name="editUser" value="Edit">
-
-
-
 
 	<?php
 		$check1="";
 		$check2="";
-		if($info['STATUS']==1){
+		if($info['userType']==1){
 			$check1="checked";
 		}
 		else{
@@ -29,10 +34,10 @@
 		}
 	?>
 	<div>
-		Status <input type="radio" name="status" value="1"<?php echo "$check1"?> > Enabled
-		<input type="radio" name="status" value="0" <?php echo "$check2"?>> Disabled
+		User Type <input type="radio" name="status" value="1"<?php echo "$check1"?> > Admin
+		<input type="radio" name="status" value="0" <?php echo "$check2"?>> User
 	</div>
-	<input type="submit" name="save" value="Update">
+	<input type="submit" name="save" value="Edit">
 
 </form>
 
