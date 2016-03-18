fbc999274350098e8d48939d009cda3b1654f07e
ephraimayite <ephraimayite@gmail.com>
2016-03-18 01:19:26 +0000

    modified file

--- tools.php
+++ tools.php
@@ -1,40 +1,40 @@
-<?php
-include_once("inventory.php");
-	class Tools extends inventory{
-		/**
-		*constructor
-		*/
-		 function _constructor(){
-		 return $this->inventory();
+	<?php
+	include_once("inventory.php");
+		class Tools extends inventory{
+			/**
+			*constructor
+			*/
+			 function _constructor(){
+			 return $this->inventory();
+		 }
+		 
+		 /**
+		 *method to add a new tool in the database
+		 *@param[all the fields in the table]
+		 *@return boolean
+		 */
+		 function addTool($InventoryName,$Quantity,$supplierID,$InventoryType,$InventoryCategory){
+			 $result=$this->addNewInventory($InventoryName,$Quantity,$supplierID,$InventoryType,$InventoryCategory);
+			 return $result;
+		 }
+		 
+		  /**
+		 *method to edit a new tool in the database
+		 *@param[all the fields in the table]
+		 *@return boolean
+		 */
+		 function editTool($primarykey,$InventoryName,$Quantity,$supplierID,$InventoryType,$InventoryCategory){
+			 $result=$this->editInventory($primarykey,$InventoryName,$Quantity,$supplierID,$InventoryType,$InventoryCategory);
+			 return $result;
+		 }
+		 
+		 function deleteTool(){}
+		 
+		 function searchTool(){}
+		 
 	 }
-	 
-	 /**
-	 *method to add a new tool in the database
-	 *@param[all the fields in the table]
-	 *@return boolean
-	 */
-	 function addTool($InventoryName,$Quantity,$supplierID,$InventoryType,$InventoryCategory){
-		 $result=$this->addNewInventory($InventoryName,$Quantity,$supplierID,$InventoryType,$InventoryCategory);
-		 return $result;
-	 }
-	 
-	  /**
-	 *method to edit a new tool in the database
-	 *@param[all the fields in the table]
-	 *@return boolean
-	 */
-	 function editTool($primarykey,$InventoryName,$Quantity,$supplierID,$InventoryType,$InventoryCategory){
-		 $result=$this->editInventory($primarykey,$InventoryName,$Quantity,$supplierID,$InventoryType,$InventoryCategory);
-		 return $result;
-	 }
-	 
-	 function deleteTool(){}
-	 
-	 function searchTool(){}
-	 
- }
+			
+			
 		
-		
-	
 
-?>
+	?>
