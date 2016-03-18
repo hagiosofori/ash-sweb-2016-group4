3e850c5a84da273e8f6c4fbc1a40914aeaff677d
[KBoohene] <[kwabena.boohene@ashesi.edu.gh]>
2016-03-18 00:28:26 +0000

    properly commented userDel code

--- userDel.php
+++ userDel.php
@@ -1,11 +1,14 @@
 <?php
+//Included the users class
 include_once("users.php");
 
+//Stores the user's ID
  if(isset($_REQUEST['userID'])){
 	$delUser = new users();
 	$result=$delUser->deleteUser($_REQUEST['userID']);
 }
 
+//Relocates to the display user page
 header("Location:displayUser.php?userType=1");
 exit();
 
