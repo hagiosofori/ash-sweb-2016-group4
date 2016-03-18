9e9378eb87252dd6d8345a490355924a3fefa399
[KBoohene] <[kwabena.boohene@ashesi.edu.gh]>
2016-03-18 00:22:44 +0000

    commented the updateUser code

--- updateUser.php
+++ updateUser.php
@@ -1,9 +1,16 @@
 <?php
+
+//Includes the user's class
 include_once("users.php");
+
+//Creates a user object
 $user = new users();
+
 if(!isset($_REQUEST['username'])){
   exit();		//if no data, exit
 }
+
+//Stores the updated user information
 if(isset($_REQUEST['username'])){
 
 $userID =$_REQUEST['userID'];
@@ -13,10 +20,11 @@ $lastname=$_REQUEST['lastname'];
 $pword=$_REQUEST['password'];
 $userType=$_REQUEST['userType'];
 
-
-
+//Calls the edit user method and passes the user information
 $result=$user->editUser($userID,$username,$firstname,$lastname,$pword,$userType);
 }
+
+//redirects to the displayUser page
 header("Location:displayUser.php?userType=1");
 exit();
 ?>
