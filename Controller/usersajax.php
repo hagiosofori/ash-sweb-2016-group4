2261f0c8c4b5ece2f46f7848630a75b056b82ae7
[KBoohene] <[okwabse@gmail.com]>
2016-04-15 21:49:56 +0000

    added ajax controller

--- Controller/usersajax.php
+++ Controller/usersajax.php
@@ -11,29 +11,19 @@
 			editUserName();		//if cmd=1 the call delete
 			break;
     case 2:
-      deleteUser();
+      editFirstName();
       break;
+		case 3:
+			editLastName();
+			break;
+		case 4:
+		  deleteUser();
+		  break;
 		default:
 			echo "wrong cmd";	//change to json message
 			break;
 	}
 
-  function deleteUser(){
-    if(!isset($_REQUEST['uc'])){
-      echo "usercode is not given";
-      exit();
-    }
-
-    $Id=$_REQUEST['uc'];
-    include("../Model/users.php");
-    $user=new users();
-    //delete the user
-    if($user->deleteUser($Id)){
-      echo "User deleted";
-    }else{
-      echo "User was not deleted.";
-    }
-  }
 
   function editUserName(){
     include_once("../Model/users.php");
@@ -46,7 +36,7 @@
 
 		$user = new users();
 
-		$verify = $user->editUserName($username,$usercode);
+		$verify = $user->editName(1,$username,$usercode);
 		if($verify==true){
 			echo"User Changed";
 		}
@@ -54,3 +44,60 @@
 			echo"User not changed";
 		}
   }
+
+	function editFirstName(){
+    include_once("../Model/users.php");
+
+		if(!isset($_REQUEST['uc'])){
+			echo"No user information";
+		}
+		$usercode=$_REQUEST['uc'];
+		$firstname=$_REQUEST['firstname'];
+
+		$user = new users();
+
+		$verify = $user->editName(2,$firstname,$usercode);
+		if($verify==true){
+			echo"User Changed";
+		}
+		else{
+			echo"User not changed";
+		}
+  }
+
+	function editLastName(){
+		include_once("../Model/users.php");
+
+		if(!isset($_REQUEST['uc'])){
+			echo"No user information";
+		}
+		$usercode=$_REQUEST['uc'];
+		$lastname=$_REQUEST['lastname'];
+
+		$user = new users();
+
+		$verify = $user->editName(3,$lastname,$usercode);
+		if($verify==true){
+			echo"User Changed";
+		}
+		else{
+			echo"User not changed";
+		}
+	}
+
+	  function deleteUser(){
+	    if(!isset($_REQUEST['uc'])){
+	      echo "usercode is not given";
+	      exit();
+	    }
+
+	    $Id=$_REQUEST['uc'];
+	    include("../Model/users.php");
+	    $user=new users();
+	    //delete the user
+	    if($user->deleteUser($Id)){
+	      echo "User deleted";
+	    }else{
+	      echo "User was not deleted.";
+	    }
+	  }
