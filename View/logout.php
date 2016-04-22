be976ae84560c1fd39963d1861b991c8395669b0
[KBoohene] <[okwabse@gmail.com]>
2016-04-22 11:43:12 +0000

    commented login animation

--- View/logout.php
+++ View/logout.php
@@ -1,21 +1,25 @@
 <html>
     <?php
+    //Session started
       session_start();
       if(!isset($_SESSION['user'])){
         header('Location:../index.php');
         exit();
       } else{
+        //Session destroyed
         session_destroy();
       }
     ?>
   <head>
     <link rel="stylesheet" type="text/css" href="../css/style.css">
-    <meta http-equiv="refresh" content="4; url=../index.php"/ >
+    <!--redirects to the login page after 3 seconds-->
     <title>Logout</title>
+    <meta http-equiv="refresh" content="4; url=../index.php"/ >
   </head>
 
-<body class="formpage">
+  <body class="formpage">
      <h1>Redirecting...</h1>
+     <!--Spinner animation-->
      <div class="spinner"></div>
   </body>
 </html>
