1c0fa58d152dc4016c2e4a9ebac9f6f8eb24b69e
ryjaled <ryjaled@gmail.com>
2016-04-22 17:47:51 +0000

    update

---
+++ Controller/delete.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+* main file responsible for all deleting in the database
+*/
+	include_once('../Model/drugs.php');
+	include_once('../Model/suppliers.php');
+	include_once('../Model/tools.php');
+
+	//take the item id, and the itemtype, and delete accordingly
+	//print_r($_REQUEST);
+
+	//retrieving id and object type from request array, and storing in variables
+	if(isset($_REQUEST['id'])){
+		$id = $_REQUEST['id'];
+		$type = $_REQUEST['item'];
+		echo $id;
+		echo $type;
+	}
+
+	//determining which table the delete query should be run on
+
+
+	//take the item id, and the itemtype, and delete accordingly
+
+	if(isset($_REQUEST['id'])){
+		$id = $_REQUEST['id'];
+		$type = $_REQUEST['item'];
+	}
+
+
+	if($type=='drug'){
+		$drug = new drugs();
+		$drug->deleteDrug($id);
+
+	}else if($type=='tool'){
+
+		echo "<br>deleting tool";
+
+
+		$tool = new tools();
+		$tool->deleteTool($id);
+
+	}else if($type=='supplier'){
+		$supplier = new suppliers();
+		$supplier->deleteSupplier($id);
+
+	}
+
+
+	//returning to home page
+	header("Location:../View/homepage.php");
+
+	//header("Location:index.php");
+
+
+?>
---
+++ Model/inventory.php
@@ -0,0 +1,91 @@
+<?php
+
+	include_once("adb.php");
+	class inventory extends adb
+	{
+
+		/**
+		* constructor
+		*/
+		function __constructor()
+		{
+
+		}
+
+		/**
+		* adds new inventory to the inventory table of database
+		* @param [all attributes/fields in the database]
+		*/
+		function addNewInventory( $InventoryName,$Quantity,$supplierID,$InventoryType,$InventoryCategory)
+		{
+			$query=null;
+			if(strcasecmp($InventoryCategory,"Tools")==0||(strpos($InventoryCategory, "Tool")!==false)){
+			$query= "insert into Tools set toolName ='$InventoryName', quantity=$Quantity,supplierID =$supplierID
+			";
+
+
+
+
+
+			}
+			else if(strcasecmp($InventoryCategory,"Drugs")==0||(strpos($InventoryCategory, "Drug")!==false)){
+			$query= "insert into drugs set drugName ='$InventoryName', drugType='$InventoryType', quantity=$Quantity,supplierID =$supplierID
+			";
+
+
+			}
+			else{
+				//does nothing
+
+			}
+
+			if($query!==null){
+			return $this->query($query);
+			}
+
+		}
+
+		/**
+		* edits already existing inventory
+		* @param [primary key of the inventory table]
+		*/
+		function editInventory($primarykey,$InventoryName,$Quantity,$supplierID,$InventoryType,$InventoryCategory)
+		{
+			$query = null;
+			if(strcasecmp($InventoryCategory,"Tools")==0||(strpos($InventoryCategory, "Tool")!==false)){
+			$query= " update Tools set toolName ='$InventoryName',  quantity=$Quantity,supplierID =$supplierID where toolId =$primarykey
+			";
+			} else if(strcasecmp($InventoryCategory,"Drugs")==0||(strpos($InventoryCategory, "Drug")!==false)){
+			$query= "update drugs set drugName ='$InventoryName', drugType='$InventoryType', quantity=$Quantity,supplierID =$supplierID where drugId=$primarykey
+			";}
+			else{
+				//does nothing
+			}
+
+
+
+			if($query!==null){
+				return $this->query($query);
+			}
+
+		}
+		/**
+		* comments here
+		*/
+		function deleteInventory( $primaryKey/*the primary key of the inventory*/)
+		{
+			$sql = "Delete from drugs where inventoryID = $primaryKey";
+			return $this->query($sql);
+		}
+
+		/**
+		* comments here
+		*/
+		function getAllInventory()
+		{
+			$sql = "select * from drugs";
+			return $this->query($sql);
+		}
+	}
+
+?>
